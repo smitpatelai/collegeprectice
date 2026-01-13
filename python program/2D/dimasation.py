02bc5dd69b153cc2f4956938361e220d3a956bf8
Smit N Patel <smitnpatel55@gmail.com>
2026-01-13 11:23:00 +0530

    updata in the code

--- python program/2D/dimasation.py
+++ python program/2D/dimasation.py
@@ -21,3 +21,45 @@ print(data[2][0])
 
 print(data[:1])
 print(data[1:])
+
+# col slicing
+print(data[:,1:])
+
+#operations on array
+print(data+3)
+
+#reshape
+arr_1 = np.array([1,2,3,4,5,6])
+reshaped = arr_1.reshape(2,3)
+print(reshaped)
+
+
+flatten = data.flatten()
+print(flatten)
+
+#np.zeros
+
+attendance = np.zeros((3,5), dtype=int)
+print(attendance)
+attendance[0][3] = 1
+attendance[1][4] = 1
+attendance[1][2] = 1
+print(attendance)
+
+salaries = np.array([
+    [15000,2000,35000],# HR
+    [15600,25000,36900],# TECh
+    [10000,50000,60400] # SALES
+])
+print(salaries)
+
+#print hr salary sum
+sumofrow = np.sum(salaries,axis=1)
+print(sumofrow[1])
+print(sumofrow[2])
+
+sumofcol = np.sum(salaries,axis=0)
+print(sumofcol)
+
+#print highest salary
+print(np.max(salaries[:,2]))
