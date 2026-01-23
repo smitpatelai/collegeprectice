60e12a955af7935ae0f57d576d8466c023243a3f
Smit N Patel <smitnpatel55@gmail.com>
2026-01-23 11:44:53 +0530

    add new data in the code

--- python program/KBC/KBC.py
+++ python program/KBC/KBC.py
@@ -10,7 +10,7 @@ def load_question(filename):
             if i + 6 < len(lines):
                 question.append({
                     "question": lines[i],
-                    "options": lines[i+1:5],
+                    "options": lines[i+1:i+5],
                     "answer": int(lines[i+5]),
                     "money": int(lines[i+6])
                 })
@@ -39,5 +39,41 @@ def play_kbc():
     totalamount = 0
     starttime = datetime.datetime.now()
     print(f"QUIZ STARTED AT: {starttime.strftime('%Y-%m-%d %H:%M:%S')}")
+
+    max_question = min(15, len(question))
+
+    for i in range(max_question):
+        q = question[i]
+
+        print(f"question: {i+1} | AMOUNT : {q['money']}")
+        print(f"{q['question']}")
+
+        for idx , b in enumerate(q["options"]):
+            print(f"{idx+1} |  {b}")
+
+        try:
+            chooice = int(input("Enter Choice In No(1-4): "))
+        except ValueError:
+            print("GAME OVER!!!!!!! Invalid Option or Answer")
+            break
+
+        if chooice == q["answer"]:
+            totalamount += q["money"]
+            bonus = int(math.sqrt(q["money"]))
+            print("Correct Answer")
+            print(f"BONUS POINTS: {bonus}")
+            print(f"Total Amount: {totalamount}")
+        else:
+            print("Game Over")
+            print(f"Current Amount: {totalamount}")
+            break
+
+    end_time = datetime.datetime.now()
+    duration = end_time - starttime
+    print("Game Over")
+    print(f"Game Ended at:{end_time}")
+    print(f"Total Duration: {duration}")
+
+    save_log(totalamount, duration)
 
 play_kbc()
