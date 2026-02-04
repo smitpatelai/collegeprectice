9e25b6f639fa99744f5c2952fb9067a86930afc3
Smit N Patel <smitnpatel55@gmail.com>
2026-02-04 17:27:39 +0530

    this is use in there file to use for the sectors

---
+++ python program/PE CHART/work7.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+sectors = ["Residential", "Commercial", "Industrial", "Agricultural","Government", "Hospitals", "Education", "Transport"]
+
+consumption = [620, 480, 710, 560, 300, 1340, 290, 410]
+
+plt.bar(sectors, consumption)
+plt.xlabel("Sector")
+plt.ylabel("Consumption")
+plt.xlabel("Sectors")
+plt.ylabel("Power Consumption (Million Units)")
+plt.title("Electricity Consumption by Sector")
+plt.xticks(rotation=10,fontsize =8)
+plt.show()
