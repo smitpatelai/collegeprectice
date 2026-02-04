b2e86b1b30bf9869fcfeb69461e8d22375aa3042
Smit N Patel <smitnpatel55@gmail.com>
2026-02-04 16:27:54 +0530

    add in the value

--- python program/PE CHART/work 2 or 2.py
+++ python program/PE CHART/work 2 or 2.py
@@ -4,10 +4,9 @@ expense_categories = ["Rent", "Food", "Transportation", "Utilities","Education",
 
 expense_values = [30, 22, 10, 8, 12, 6, 7, 5]
 
-plt.pie(expense_values,labels=expense_categories,autopct='%1.1f%%',startangle=90)
-
-plt.title("Household Monthly Expense Distribution")
-
-plt.axis('equal')
+explode = (0.05, 0, 0, 0, 0, 0, 0, 0)
+plt.pie(expense_values,labels=expense_categories,autopct='%1.1f%%',shadow=True,explode=explode)
+plt.title("Monthly Expense Distribution")
+plt.legend(title="Expense Categories", loc="upper left", bbox_to_anchor=(0.91, 1.1))
 
 plt.show()
