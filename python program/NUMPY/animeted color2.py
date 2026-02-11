9dae5428695ff68100db54bb1f58104a8d3653e7
Smit N Patel <smitnpatel55@gmail.com>
2026-02-11 16:20:19 +0530

    changes in the value

--- python program/NUMPY/animeted color2.py
+++ python program/NUMPY/animeted color2.py
@@ -4,8 +4,13 @@ import matplotlib.pyplot as plt
 bar = np.zeros(shape=(40, 200, 3), dtype=np.uint8)
 
 for progress in range(0, 200, 10):
-    bar[:] = [20, 20, 20]        # Background (dark gray)
+    bar[:] = [255, 255, 255]        # Background (dark gray)
     bar[:, :progress] = [0, 240, 0]   # Green progress
+
+    bar[:+1,:] = [0,0,0]   # top border
+    bar[:,:+1] = [0,0,0]   # Right border
+    bar[-1,:] = [0,0,0]    # Left border
+    bar[:,-1] = [0,0,0]    # Bottom border
 
     plt.imshow(bar)
     plt.axis("off")
