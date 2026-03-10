6c7cb862853b5f93ee47a616253134b7dbb67572
Smit N Patel <smitnpatel55@gmail.com>
2026-03-10 10:37:29 +0530

    new file and there data

--- python program/Streamlit/signup.py
+++ python program/Streamlit/signup.py
@@ -4,6 +4,13 @@ import os
 import re
 
 st.set_page_config(page_title="Sign UP Page")
+
+def load_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+load_css("style.css")
+
 FILE_NAME = "users.csv"
 st.header("Sign UP")
 
